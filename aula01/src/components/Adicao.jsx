06328548e9ab5573aa9325ce90ea807efd9683cb
higorsouza <higor.andrade@hotmail.com>
2024-08-28 12:37:26 -0300

    alterações da segunda aula

---
+++ aula01/src/components/Adicao.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Adicao = (props) => {
+  return (
+    <h1>{num1+num2}</h1>
+  )
+}
+
+export default Adicao
---
+++ aula01/src/components/PrecisoEstudar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrecisoEstudar = (props) => {
+  return (
+    <div>Preciso estudar: {nomeDaTecnologia}</div>
+  )
+}
+
+export default PrecisoEstudar
